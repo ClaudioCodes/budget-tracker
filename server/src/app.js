75edcbb9504566cd85872d955a12be81a2d31e6a
Claudio Lespier <clespier@gmail.com>
2024-08-12 10:45:00 -0400

    Added users controller and route.

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const dbConnect = require('./config/dbConnect');
+const { registerUser } = require("./controllers/users/usersCtrl");
+const userRoute = require('./routes/users/usersRoute');
+
 const app = express();
 
 //dbConnect
 dbConnect();
+
+//routes
+app.use('/', userRoute)
+
+
+
+
 module.exports = app;
