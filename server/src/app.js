32e4b7a634e360c096de38ad35c7d91bf3a36d02
Claudio Lespier <clespier@gmail.com>
2024-08-12 12:11:56 -0400

    Added error handler.

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dbConnect = require('./config/dbConnect');
+const { errorHandler } = require("./middlewares/errorMiddleware");
 const { registerUser } = require("./controllers/users/usersCtrl");
 const userRoute = require('./routes/users/usersRoute');
 
@@ -8,10 +9,14 @@ const app = express();
 //dbConnect
 dbConnect();
 
+//middlewares
+app.use(express.json())
+
 //routes
-app.use('/', userRoute)
+app.use('/', userRoute)  
 
-
+//Error
+app.use(errorHandler)
 
 
 module.exports = app;
--- server/src/controllers/users/usersCtrl.js
+++ server/src/controllers/users/usersCtrl.js
@@ -1,6 +1,21 @@
+const expressAsyncHandler = require('express-async-handler');
+const User = require("../../model/User");
+
+
 //Register
-const registerUser = (req, res) => {
-    res.json({user:'admin'});
-};
+const registerUser = expressAsyncHandler(async (req, res) => {
+    const { email, firstName, lastName, password } = req?.body;
+    //check if user exists
+    const userExists = await User.findOne({ email: req.body.email });
+if(userExists) throw new Error('User already exists');
+        
+    try{
+        const user = await User.create({ email, firstName, lastName, password });
+        res.status(200).json(user);
+    } catch (error) {
+        res.json(error)
+    }
+});
+
 
 module.exports = { registerUser }
